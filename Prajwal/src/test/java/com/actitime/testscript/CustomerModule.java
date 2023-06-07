af1f3c91ed69ecadfc26d05d1af5b6673dd39ab8
DELL <DELL@DESKTOP-EFLV044>
2023-06-07 14:07:25 +0530

    mmm

--- Prajwal/src/test/java/com/actitime/testscript/CustomerModule.java
+++ Prajwal/src/test/java/com/actitime/testscript/CustomerModule.java
@@ -1,5 +1,7 @@
 package com.actitime.testscript;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
@@ -11,9 +13,13 @@ public class CustomerModule {
 	}
 	
 @Test
-public void createCust() {
+public void createCust() throws InterruptedException {
 	Reporter.log("ashdsadj",true);
 	WebDriver driver =new ChromeDriver();
 	driver.get("https://demo.actitime.com");
+	driver.findElement(By.id("username")).sendKeys("admin");
+	driver.findElement(By.name("pwd")).sendKeys("manager"+Keys.ENTER);
+	Thread.sleep(5000);
+	driver.close();
 }
 }
