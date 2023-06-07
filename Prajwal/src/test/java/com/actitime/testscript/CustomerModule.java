7b780580b2f30557bff0bcef64dbb004ac46bd25
DELL <DELL@DESKTOP-EFLV044>
2023-06-07 13:55:46 +0530

    sss

--- Prajwal/src/test/java/com/actitime/testscript/CustomerModule.java
+++ Prajwal/src/test/java/com/actitime/testscript/CustomerModule.java
@@ -1,11 +1,19 @@
 package com.actitime.testscript;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class CustomerModule {
+	static {
+		System.setProperty("webdriver.chrome.driver", "./src/main/resources/driver/chromedriver.exe");
+	}
+	
 @Test
 public void createCust() {
 	Reporter.log("ashdsadj",true);
+	WebDriver driver =new ChromeDriver();
+	driver.get("https://demo.actitime.com");
 }
 }
